2523405d392d8cee85f08482bc02a9b37d3116fa
jimosk <jimosdk@gmail.com>
2021-08-31 19:32:50 +0300

    Implemented helper methods log_in!,log_out!(used for logging in or out valid users),require_current_user,require_unsigned_user (used in callbacks) and current_user(returns the signed in user if there is one ,otherwise returns nil)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user 
+        return nil unless session[:session_token]
+        User.find_by(session_token: session[:session_token])
+    end
+
+    def log_in!
+        session[:session_token] = @user.reset_session_token!
+    end
+
+    def log_out!
+        current_user.reset_session_token!
+        session[:session_token] = nil 
+    end
+
+    def require_current_user
+        redirect_to new_session_url unless current_user
+    end
+
+    def require_unsigned_user
+        redirect_to movies_url if current_user
+    end
 end
