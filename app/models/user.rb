973e03b5d6c76b2e2e1220fd810723751c96c6c8
jimosk <jimosdk@gmail.com>
2021-09-01 22:40:10 +0300

    Implemented has many association for fetching a Users movie posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,10 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token
 
+    has_many :posts,
+        foreign_key: :poster_id,
+        class_name: :Movie
+
    def self.find_by_credentials(username,password)
         user = User.find_by(username: username)
         user && user.is_password?(password) ? user : nil 
