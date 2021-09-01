7c367d6e0b1aabc7cae3f48da4ea527f952aa4f3
jimosk <jimosdk@gmail.com>
2021-09-01 23:16:05 +0300

    Implemented helper method , creating  description text for the reset button of a sorting filter.(For visual clarity on the corresponding page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     def auth_token
        "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe
     end
+
+    def reset_sort_filter_button_text
+        #helper method returning descriptive text on a reset button
+        
+        sorting_field = params[:filter][:sorting_field] == 'created_at' ? 'date' : 
+                                                                  params[:filter][:sorting_field]
+        order = params[:filter][:sorting_order] == 'asc' ? 'ascending' : 'descending'
+
+        "Posts sorted by #{sorting_field},in #{order} order"
+    end
 end
