30f4f1d780d5721cbd0f8c10b53c936305078c0f
jimosk <jimosdk@gmail.com>
2021-09-04 17:54:27 +0300

    Modified seed sample using gem faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ require 'faker'
 ActiveRecord::Base.transaction do 
     User.destroy_all
 
-    10.times do 
+    30.times do 
         begin
            User.create!(username: Faker::Name.name,password:'123456') 
         rescue #=> exception
@@ -18,19 +18,18 @@ ActiveRecord::Base.transaction do
     
     users = User.all
 
-    15.times do 
+    5.times do 
         begin
            Movie.create!(title: Faker::Movie.title,description:'Lorem ipsum dolor sit amet, 
                     consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar 
-                    ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. 
-                    Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',poster_id:users.sample.id) 
+                    ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.',poster_id:users.sample.id) 
         rescue #=> exception
         end
     end
 
     users.each do |user|
         Movie.all.each do |movie|
-            next unless rand(1..10) > 7
+            next unless rand(1..10) < 7
             begin
                 Rating.create!(post_id: movie.id,user_id:user.id,value: ['like','hate'].sample)
             rescue 
