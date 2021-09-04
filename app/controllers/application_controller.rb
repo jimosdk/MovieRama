2c49a5e9144115fbf75421a236c23e3eff78aec0
jimosk <jimosdk@gmail.com>
2021-09-04 18:04:33 +0300

    Implemented helper method current_user_preload ,returns current user (if any) same as the pre existing current user method,but it also preloads associations :posts and :ratings for the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user,:filter_params
+    helper_method :current_user,:current_user_preload,:filter_params
 
     def current_user 
         #Finds the signed up user making the requests,if any
         return nil unless session[:session_token]
         User.find_by(session_token: session[:session_token])
+    end
+
+    def current_user_preload
+        #Finds the signed up user making the requests,if any
+        #Also preloads ratings and posts to deal with N+1 queries
+        return nil unless session[:session_token]
+        User.includes(:ratings,:posts).
+        find_by(session_token: session[:session_token])
     end
 
     def log_in!
