721dcf2680ef706d3ed32c6940ef04a834fc93fa
jimosk <jimosdk@gmail.com>
2021-09-04 13:02:01 +0300

    Included configurations for precompliling stylesheet assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module MovieRama
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+
+    config.assets.precompile += %w( article_section.css )
+    config.assets.precompile += %w( filter_bar.css )
+    config.assets.precompile += %w( form.css )
+    config.assets.precompile += %w( header.css )
+    config.assets.precompile += %w( new_movie_button.css )
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
